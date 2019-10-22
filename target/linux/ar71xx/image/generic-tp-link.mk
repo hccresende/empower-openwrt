3be3d1988f84a12c56c39d6785bf7bbb07f6a2b8
joncnet <g.baggio@create-net.org>
2019-10-22 09:31:04 +0200

    Update generic-tp-link.mk

--- target/linux/ar71xx/image/generic-tp-link.mk
+++ target/linux/ar71xx/image/generic-tp-link.mk
@@ -310,6 +310,7 @@ TARGET_DEVICES += tl-wdr3600-v1
 define Device/tl-wdr4300-v1
   $(Device/tl-wdr3600-v1)
   DEVICE_TITLE := TP-LINK TL-WDR4300 v1
+  DEVICE_PACKAGES := empower-lvap-agent openvswitch luci
   TPLINK_HWID := 0x43000001
 endef
 TARGET_DEVICES += tl-wdr4300-v1
